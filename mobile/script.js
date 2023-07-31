6d91d3f6c466c8b7c0e66a9e1a06bae3a090ebb4
Aethereux <euclidguillermo28@gmail.com>
2023-07-31 15:41:25 +0700

    Fixes

--- mobile/script.js
+++ mobile/script.js
@@ -3,7 +3,16 @@ const dropdownToggle = document.querySelector('.dropdown > a'); // Select the SE
 const dropdownContent = document.querySelector('.dropdown-content');
 
 dropdownToggle.addEventListener('click', () => {
+  // Toggle the 'show' class for the dropdown content
   dropdownContent.classList.toggle('show');
+  
+  // Hide other open dropdowns
+  const dropdowns = document.querySelectorAll('.dropdown-content');
+  dropdowns.forEach((dropdown) => {
+    if (dropdown !== dropdownContent) {
+      dropdown.classList.remove('show');
+    }
+  });
 });
 
 // Close the dropdown if clicked outside of it
