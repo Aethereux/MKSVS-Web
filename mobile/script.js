3ebb1d27dcf34490766759942458b40e45d521a3
Aethereux <euclidguillermo28@gmail.com>
2023-07-31 15:48:49 +0700

    Fixes

--- mobile/script.js
+++ mobile/script.js
@@ -2,17 +2,10 @@
 const dropdownToggle = document.querySelector('.dropdown > a'); // Select the SERVICES link directly
 const dropdownContent = document.querySelector('.dropdown-content');
 
-dropdownToggle.addEventListener('click', () => {
-  // Toggle the 'show' class for the dropdown content
+// Show the dropdown content when clicking on the "SERVICES" link
+dropdownToggle.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent the default link behavior (navigating to the href)
   dropdownContent.classList.toggle('show');
-  
-  // Hide other open dropdowns
-  const dropdowns = document.querySelectorAll('.dropdown-content');
-  dropdowns.forEach((dropdown) => {
-    if (dropdown !== dropdownContent) {
-      dropdown.classList.remove('show');
-    }
-  });
 });
 
 // Close the dropdown if clicked outside of it
