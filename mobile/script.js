26e7720368e66f489c17fb324d1082f74e574219
Aethereux <euclidguillermo28@gmail.com>
2023-08-01 08:47:12 +0700

    Changes

--- mobile/script.js
+++ mobile/script.js
@@ -1,16 +1,14 @@
-// JavaScript to handle the dropdown on click for both desktop and mobile screens
-const dropdownToggle = document.querySelector('.dropdown > a'); // Select the SERVICES link directly
+// JavaScript to handle the dropdown on click for smaller screens
+const dropdownToggle = document.querySelector('.dropdown a');
 const dropdownContent = document.querySelector('.dropdown-content');
 
-// Show the dropdown content when clicking on the "SERVICES" link
-dropdownToggle.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default link behavior (navigating to the href)
+dropdownToggle.addEventListener('click', () => {
   dropdownContent.classList.toggle('show');
 });
 
 // Close the dropdown if clicked outside of it
 window.addEventListener('click', (event) => {
-  if (!event.target.closest('.dropdown')) {
+  if (!event.target.matches('.dropdown a')) {
     dropdownContent.classList.remove('show');
   }
 });
