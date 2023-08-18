930f30520dab01e99d07a5e3ef818f2d873b29fa
Aethereux <euclidguillermo28@gmail.com>
2023-08-18 10:42:50 +0700

    Adjustments

--- script.js
+++ script.js
@@ -17,17 +17,6 @@ servicesDropdownToggle.addEventListener('click', (event) => {
   }
 });
 
-// Close the mobile menu and dropdown if clicked outside of them
-document.addEventListener('click', (event) => {
-  if (!event.target.matches('.menu-icon') && !event.target.closest('.navbar')) {
-    mobileMenu.classList.remove('active');
-  }
-
-  if (!event.target.closest('.dropdown')) {
-    dropdownContent.classList.remove('show');
-  }
-});
-
 // Sliding shts
 const images = document.querySelectorAll('.sliding-container img');
 let index = 0;
